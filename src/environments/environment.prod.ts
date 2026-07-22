1937106d1b02beaf36ef1eadcb78fe12f2cec812
Saravanan K <saravananmcase@gmail.com>
2026-07-22 17:24:01 +0530

    update the api url
    updated the api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   gstEnabled: false,
   businessName: 'Marudham Hardwares',
-  apiUrl: 'https://invoicingsytem.onrender.com/invoicesys' // Leave empty! We will inject this via Docker/env vars on Render
+  apiUrl: 'https://invoicingsytem.onrender.com/invoicesys/api' // Leave empty! We will inject this via Docker/env vars on Render
 };
